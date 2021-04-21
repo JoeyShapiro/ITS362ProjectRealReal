ff674a12faf288300cf5a4cbca278bcb4deb7721
Joey Shapiro <joeyashapiro@gmail.com>
2021-04-21 02:16:22 -0400

    homepage work

--- index.php
+++ index.php
@@ -15,5 +15,34 @@
         // do stuff
         echo('hello ' . $user['username']);
     }
+?>
+
+<div id="total">
+<?php
+    $stmt = $db->prepare("SELECT COUNT(*) as total FROM users");
+    $stmt->bind_param();
+    $stmt->execute();
+    $result = $stmt->get_result();
+    $row = $result->fetch_assoc();
+
+    echo('<h3>' . $row['total'] . ' Total Users</h3>');
+?>
+</div>
+
+<div id="motd">
+    <h3>Message of the Day:</h3>
+    <h4>
+<?php
+    echo('Today is ' . date('l jS \of F Y h:i:s A') . '.');
+?>
+    </h4>
+    <h4>
+<?php
+    echo('GLHF');
+?>
+    </h4>
+</div>
+
+<?php
     require('html/footer.html');
 ?>
